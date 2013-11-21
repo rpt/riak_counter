a013a1c88532a6d299b8cb94ff1c91c370c078da
Krzysztof Rutka <krzysztof.rutka@gmail.com>
2013-11-21 08:23:26 +0100

    Add parallel unit test

--- test/riak_counter_tests.erl
+++ test/riak_counter_tests.erl
@@ -8,7 +8,8 @@
 
 counter_test_() ->
     {setup, fun setup/0, fun teardown/1,
-     [fun delete_bug/0]}.
+     [{timeout, 30, fun parallel_reads/0},
+      fun delete_bug/0]}.
 
 setup() ->
     {ok, Pid} = riak_counter:start_link(5),
@@ -21,9 +22,32 @@ teardown(Pid) ->
 
 %% Tests ----------------------------------------------------------------------
 
+parallel_reads() ->
+    N = 100,
+    Pid = self(),
+    ok = riak_counter:reset(?COUNTER),
+    spawn(fun() ->
+              [ok = riak_counter:update(?COUNTER) || _ <- lists:seq(1, N)],
+              Pid ! updates_done
+          end),
+    ok = wait_for_updates(),
+    ?assertEqual({ok, N}, riak_counter:read(?COUNTER)).
+
 delete_bug() ->
     ok = riak_counter:update(?COUNTER),
     ok = riak_counter:update(?COUNTER),
     ok = riak_counter:delete(?COUNTER),
     ok = riak_counter:update(?COUNTER),
     ?assertEqual({ok, 1}, catch riak_counter:read(?COUNTER)).
+
+%% Helper functions -----------------------------------------------------------
+
+wait_for_updates() ->
+    receive
+        updates_done ->
+            ok
+    after
+        1 ->
+            {ok, _} = riak_counter:read(?COUNTER),
+            wait_for_updates()
+    end.
